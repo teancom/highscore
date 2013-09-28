ad1e9de35865fd75b653f4c637f9588dc9eb70f6
David Bishop <david@gnuconsulting.com>
2013-09-27 22:48:40 -0400

    initial import

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Highscore::Application.config.secret_key_base = '538daa7f5bfcd24f4fb9af995481b9dc8d0cbc53dc89dae3e0366cc4e671d013a1bd4011ef0439292c63d4a69f7aa4b62547b73737e4c6391ed6f2c013dc82bd'
