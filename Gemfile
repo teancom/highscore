0272813255752104a645cd5f6b0e8fc057ac65c1
David Bishop <david@gnuconsulting.com>
2013-11-15 12:14:14 -0500

    add airbrake support

--- Gemfile
+++ Gemfile
@@ -45,3 +45,4 @@ end
 # gem 'debugger', group: [:development, :test]
 
 gem 'newrelic_rpm'
+gem 'airbrake'
---
+++ config/initializers/airbrake.rb
@@ -0,0 +1,3 @@
+Airbrake.configure do |config|
+  config.api_key = '53e432327ee5357a7bae89d23ba69d45'
+end
